7995bba696ab626c16b140e6363aab41449261f7
Vatsal Chauhan <112819416+Vatsal-Chauhan024@users.noreply.github.com>
2025-08-24 16:51:34 +0530

    fix: removed json file

---
+++ .commitlintrc.mjs
@@ -0,0 +1,7 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'custom']],
+    'subject-case': [0],
+  },
+};
